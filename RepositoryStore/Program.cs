5d369ffc86180e6734d82434547680fd6e0a6ebf
Eder Vieira de Oliveira Ribeiro <eder.edy@gmail.com>
2025-03-21 16:26:01 -0400

    Criacao de repository e padrão pattern

--- RepositoryStore/Program.cs
+++ RepositoryStore/Program.cs
@@ -1,13 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using RepositoryStore.Data;
+using RepositoryStore.Repositories;
+using RepositoryStore.Repositories.Abstractions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 	options.UseNpgsql(builder.Configuration.GetConnectionString("defaultConnection")));
 
+builder.Services.AddTransient<IProductRepository,ProductRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/v1/products", () => "Hello World!");
+app.MapPost("/v1/products", () => "Hello World!");
+app.MapPut("/v1/products", () => "Hello World!");
+app.MapDelete("/v1/products", () => "Hello World!");
 
 app.Run();
